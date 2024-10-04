0749c545a422bf5b27d4d636dba518cad87ab4ab
Joakim Junnila <joakim.junnila@student.oulu.fi>
2024-10-04 13:19:51 +0300

    Part 2.11 complete

--- part2/Puhelinluettelo/src/App.jsx
+++ part2/Puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, update}) => {
   return (
@@ -25,15 +26,19 @@ const Persons = ({persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [currentFilter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        console.log("Response: ", response);
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()

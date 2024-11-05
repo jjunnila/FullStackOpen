5a794d2a818b7ec9b0f78abb6a0e6acc94324339
Joakim Junnila <joakim.junnila@student.oulu.fi>
2024-11-05 12:30:36 +0200

    Part 5.17 complete

---
+++ part5/end-to-end/tests/bloglist.test.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Bloglist app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to the application')).toBeVisible()
+    await expect(page.getByText('Blogs')).not.toBeVisible()
+  })
+})
